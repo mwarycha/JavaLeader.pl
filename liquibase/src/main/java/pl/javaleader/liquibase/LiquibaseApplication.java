f9617e546530c7c3f02a5937c899081dded0f63a
Marcin Warycha <marcin.warycha@ericsson.com>
2019-07-19 12:58:24 +0200

    add liquibase

---
+++ liquibase/src/main/java/pl/javaleader/liquibase/LiquibaseApplication.java
@@ -0,0 +1,11 @@
+package pl.javaleader.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseApplication.class, args);
+	}
+}
