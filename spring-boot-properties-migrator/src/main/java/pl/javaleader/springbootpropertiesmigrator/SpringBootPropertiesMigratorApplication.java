08229c4dee08f57d776c9a79687820260913f886
Marcin Warycha <marcin.warycha@ericsson.com>
2019-08-04 14:55:36 +0200

    add spring-boot-properties-migrator

---
+++ spring-boot-properties-migrator/src/main/java/pl/javaleader/springbootpropertiesmigrator/SpringBootPropertiesMigratorApplication.java
@@ -0,0 +1,12 @@
+package pl.javaleader.springbootpropertiesmigrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPropertiesMigratorApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPropertiesMigratorApplication.class, args);
+	}
+
+}
