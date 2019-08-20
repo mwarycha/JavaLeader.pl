731916bd2e86a430d2b51d29b6f11760d3731884
Marcin Warycha <marcin.warycha@ericsson.com>
2019-08-20 21:17:59 +0200

    remove unused files

--- bingo-app/api/src/main/java/service/BingoService.java
+++ bingo-app/api/src/main/java/service/BingoService.java
@@ -1,7 +1,6 @@
 package service;
 
 import javafx.util.Pair;
-
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -12,8 +11,7 @@ public interface BingoService {
     int generateRandomNumberFromSpecificRange(int min, int max);
     StringBuilder getStringBuilderBingo5x5Card(int [][] arrayToBePrinted);
 
-    Pair<Integer, int [][]> getWinner(
-                                      int cardAmountToTest,
+    Pair<Integer, int [][]> getWinner(int cardAmountToTest,
                                       Map<Integer, int [][] > mapBasicBingoCardCardIndexAndCardArray,
                                       Set<Integer> allGeneratedNumbersSet,
                                       List<Pair<Integer, Integer>> listOfWinnerIndexes);
