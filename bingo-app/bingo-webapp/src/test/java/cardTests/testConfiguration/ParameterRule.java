cb3c8c48d102374d6c8e2155021afb02c50b22f7
Marcin Warycha <marcin.warycha@ericsson.com>
2019-08-20 16:49:21 +0200

    refactoring remove PrinteHelper

--- bingo-app/bingo-webapp/src/test/java/cardTests/testConfiguration/ParameterRule.java
+++ bingo-app/bingo-webapp/src/test/java/cardTests/testConfiguration/ParameterRule.java
@@ -1,17 +1,21 @@
 package cardTests.testConfiguration;
 
-import helpers.PrinterHelper;
 import javafx.util.Pair;
 import org.junit.rules.MethodRule;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.Statement;
+import service.BingoCardServiceImpl;
+
 import java.util.List;
+import java.util.logging.Logger;
 
 public class ParameterRule implements MethodRule {
 
     private int parameterIndex = 0;
 
     public  List<List<Pair<Integer, Integer>>> parameters;
+
+    static final Logger logger = Logger.getLogger(BingoCardServiceImpl.class.getName());
 
     public ParameterRule(List<List<Pair<Integer, Integer>>> someParameters){
         parameters = someParameters;
@@ -26,7 +30,7 @@ public class ParameterRule implements MethodRule {
         return new Statement (){
             public void evaluate(){
                 for (int i = 0; i < parameters.size(); i++){
-                    PrinterHelper.printLog("\nwinners pattern indexes" + parameters.get(i).toString() + "\n");
+                    logger.info("\nwinners pattern indexes" + parameters.get(i).toString() + "\n");
                     parameterIndex = i;
                     try {
                         statement.evaluate();
