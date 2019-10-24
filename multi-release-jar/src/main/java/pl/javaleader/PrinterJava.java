ddac838c90dcfe3c7ff64dafae0e168cd53d3337
Marcin Warycha <marcin.warycha@ericsson.com>
2019-10-24 12:31:05 +0200

    add multi-release-jar

---
+++ multi-release-jar/src/main/java/pl/javaleader/PrinterJava.java
@@ -0,0 +1,13 @@
+package pl.javaleader;
+
+public class PrinterJava {
+
+    public static void printMsg(){
+        System.out.println("PrinterJava8");
+    }
+
+    public static void main(String[] args) {
+        printMsg();
+    }
+
+}
---
+++ multi-release-jar/src/main/java9/pl/javaleader/PrinterJava.java
@@ -0,0 +1,12 @@
+package pl.javaleader;
+
+public class PrinterJava {
+    public static void printMsg(){
+        System.out.println("PrinterJava9");
+    }
+
+    public static void main(String[] args) {
+        printMsg();
+    }
+
+}
