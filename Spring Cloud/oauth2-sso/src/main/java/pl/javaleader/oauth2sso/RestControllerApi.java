eb2c64f19c62db8d2acecf6bbf14d1c51f078b6e
Marcin Warycha <marcin.warycha@ericsson.com>
2019-12-03 08:48:30 +0100

    add token endpoint generated by github

--- Spring Cloud/oauth2-sso/src/main/java/pl/javaleader/oauth2sso/RestControllerApi.java
+++ Spring Cloud/oauth2-sso/src/main/java/pl/javaleader/oauth2sso/RestControllerApi.java
@@ -1,10 +1,20 @@
 package pl.javaleader.oauth2sso;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestControllerApi {
+
+    @Autowired
+    private OAuth2ClientContext clientContext;
+
+    @GetMapping("/token")
+    String showToken() {
+        return clientContext.getAccessToken().getValue();
+    }
 
     @GetMapping("/public")
     String getPublicContent(){
