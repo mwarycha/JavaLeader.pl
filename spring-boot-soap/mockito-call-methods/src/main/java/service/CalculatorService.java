d65150a2e83fb506f577bb2776c761df06b6d43f
Marcin Warycha <marcin.warycha@ericsson.com>
2020-01-13 08:56:40 +0100

    delete project from bad location

--- spring-boot-soap/mockito-call-methods/src/main/java/service/CalculatorService.java
+++
@@ -1,8 +0,0 @@
-package service;
-
-public interface CalculatorService {
-    double add(double numberA, double numberB);
-    double subtract(double numberA, double numberB);
-    double multiply(double numberA, double numberB);
-    double divide(double numberA, double numberB);
-}
