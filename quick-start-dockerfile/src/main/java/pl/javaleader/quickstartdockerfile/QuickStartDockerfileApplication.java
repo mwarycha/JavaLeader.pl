1677f735926718814bc9dec9ac195063fc491e54
Marcin Warycha <marcin.warycha@ericsson.com>
2020-02-04 21:31:24 +0100

    add quick-start-dockerfile

---
+++ quick-start-dockerfile/src/main/java/pl/javaleader/quickstartdockerfile/QuickStartDockerfileApplication.java
@@ -0,0 +1,13 @@
+package pl.javaleader.quickstartdockerfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickStartDockerfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickStartDockerfileApplication.class, args);
+	}
+
+}
