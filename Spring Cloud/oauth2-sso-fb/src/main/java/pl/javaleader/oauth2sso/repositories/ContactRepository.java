1e227aa9689789667183cb2ae68eb7feb1a8cf4b
Marcin Warycha <marcin.warycha@ericsson.com>
2020-02-22 17:04:04 +0100

    refactoring

--- Spring Cloud/oauth2-sso-fb/src/main/java/pl/javaleader/oauth2sso/repositories/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.javaleader.oauth2sso.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.javaleader.oauth2sso.model.Contactdata;
-
-public interface ContactRepository extends CrudRepository<Contactdata, String> {
-}
