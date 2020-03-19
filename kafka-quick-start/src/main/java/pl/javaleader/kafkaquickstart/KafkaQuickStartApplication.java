efa2e8ed1241b9401ff0ecab5f3901afcfe687a3
Marcin Warycha <marcin.warycha@ericsson.com>
2020-03-19 20:35:23 +0100

    add kafka quickstart

---
+++ kafka-quick-start/src/main/java/pl/javaleader/kafkaquickstart/KafkaQuickStartApplication.java
@@ -0,0 +1,13 @@
+package pl.javaleader.kafkaquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaQuickStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaQuickStartApplication.class, args);
+	}
+
+}
---
+++ kafka-quick-start/src/test/java/pl/javaleader/kafkaquickstart/KafkaQuickStartApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javaleader.kafkaquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaQuickStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
