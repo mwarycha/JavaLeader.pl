9e58a1287eaf4a786b86475acaf0cb5baec4887b
Marcin Warycha <mwarycha@gmail.com>
2020-04-12 18:21:06 +0200

    add processor

--- cxf-wsdl-to-java-camel/src/main/java/camel/InvokeSoapServiceRouteBuilder.java
+++ cxf-wsdl-to-java-camel/src/main/java/camel/InvokeSoapServiceRouteBuilder.java
@@ -1,5 +1,7 @@
 package camel;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.cxf.common.message.CxfConstants;
 
@@ -12,14 +14,22 @@ public class InvokeSoapServiceRouteBuilder extends RouteBuilder {
 
                 .bean(GetAdditionRequestBuilder.class, "getSubstraction")
 
-                .setHeader(CxfConstants.OPERATION_NAME, constant("subtraction"))
+                .setHeader(CxfConstants.OPERATION_NAME,      constant("subtraction"))
                 .setHeader(CxfConstants.OPERATION_NAMESPACE, constant("http://ws.javaleader.pl/"))
+
+                .log("ReqLog ${body}")
+
+                .process(new Processor() {
+                    public void process(Exchange e) {
+                        log.debug(e.getIn().getBody() + "body from processor");
+                    }
+                })
 
                 .to("cxf://http://localhost:8181/cxf/calcService"
                         + "?serviceClass=pl.javaleader.ws.Calculator"
                         + "&wsdlURL=http://localhost:8181/cxf/calcService?wsdl")
 
-                .log(">>> ${body}");
+                 .log("ResLog ${body}");
     }
 }
 
