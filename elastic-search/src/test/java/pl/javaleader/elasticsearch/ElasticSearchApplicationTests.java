d825dcf8b417922e79a477d149b0dac9bef5cdaf
mwary <mwarycha@gmail.com>
2021-11-06 14:36:56 +0100

    add elasticsearch

---
+++ elastic-search/src/test/java/pl/javaleader/elasticsearch/ElasticSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javaleader.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
